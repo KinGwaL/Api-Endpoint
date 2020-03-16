cb4335fa46806275aaf6d9580e3fa0b5e85b9da2
King Wa Lai <king_lai@outlook.com>
2020-03-16 15:13:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -41,6 +41,39 @@ app.post('/update', function(req, res) {
     });
 });
 
+app.post('/insert', function(req, res) {
+    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+        // watch for any connect issues
+        if (err) console.log(err);
+        conn.query(
+            'INSERT INTO salesforce.Medical__c (Name__c, Phone__c, Temperature__c, Company_Name__c) VALUES ($1, $2, $3, $4)',
+            [req.body.name.trim(), req.body.phone.trim(), req.body.temperature.trim(), req.body.companyName.trim()],
+            function(err, result) {
+                if (err != null || result.rowCount == 0) {
+                  conn.query(
+            'INSERT INTO salesforce.Medical__c (Name__c, Phone__c, Temperature__c, Company_Name__c) VALUES ($1, $2, $3, $4)',
+            [req.body.name.trim(), req.body.phone.trim(), req.body.temperature.trim(), req.body.companyName.trim()],
+                  function(err, result) {
+                    done();
+                    if (err) {
+                        res.status(400).json({error: err.message});
+                    }
+                    else {
+                        // this will still cause jquery to display 'Record updated!'
+                        // eventhough it was inserted
+                        res.json(result);
+                    }
+                  });
+                }
+                else {
+                    done();
+                    res.json(result);
+                }
+            }
+        );
+    });
+});
+
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
