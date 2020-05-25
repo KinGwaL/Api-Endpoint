9daf9051320581d7257abdecaa89727402981351
King Wa Lai <king_lai@outlook.com>
2020-05-25 12:01:33 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,43 +8,43 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 app.post('/insert', function(req, res) {
-//     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
-//         // watch for any connect issues
-//         if (err) console.log(err);
-//         conn.query(
-//             'INSERT INTO salesforce.Ruby_Survey__c (Full_Name__c, Position__c, Rating__c, Learnt_1__c, Learnt_2__c, Learnt_3__c, Suggestion_1__c, Suggestion_2__c, Suggestion_3__c) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
-//             [req.body.name.trim(), req.body.position.trim(), req.body.rating.trim(), req.body.learnt1.trim(), req.body.learnt2.trim(), req.body.learnt3.trim(), req.body.suggest1.trim(), req.body.suggest2.trim(), req.body.suggest3.trim()],
-//             function(err, result) {
-//                 done();
-//                 if (err) {
-//                     res.status(400).json({error: err.message});
-//                 }
-//                 else {
-//                     res.json(result);
-//                 }
-//             }
-//         );
-//     });
+    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+        // watch for any connect issues
+        if (err) console.log(err);
+        conn.query(
+            'INSERT INTO salesforce.Ruby_Survey__c (Full_Name__c, Position__c, Rating__c, Learnt_1__c, Learnt_2__c, Learnt_3__c, Suggestion_1__c, Suggestion_2__c, Suggestion_3__c) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
+            [req.body.name.trim(), req.body.position.trim(), req.body.rating.trim(), req.body.learnt1.trim(), req.body.learnt2.trim(), req.body.learnt3.trim(), req.body.suggest1.trim(), req.body.suggest2.trim(), req.body.suggest3.trim()],
+            function(err, result) {
+                done();
+                if (err) {
+                    res.status(400).json({error: err.message});
+                }
+                else {
+                    res.json(result);
+                }
+            }
+        );
+    });
 });
 
 app.get('/all-servey', function(req, res) {
     
-//     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
-//         if (err) console.log(err);
-//         conn.query(
-//             'SELECT * FROM salesforce.Ruby_Survey__c ORDER BY CreatedDate DESC',
-//             function(err, result) {
-//                 done();
-//                 if (err) {
-//                     res.status(400).json({error: err.message});
-//                 }
-//                 else {
-//                     //res.json(result);
-//                     res.send(res.json(result));
-//                 }
-//             }
-//         );
-//     });
+    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+        if (err) console.log(err);
+        conn.query(
+            'SELECT * FROM salesforce.Ruby_Survey__c ORDER BY CreatedDate DESC',
+            function(err, result) {
+                done();
+                if (err) {
+                    res.status(400).json({error: err.message});
+                }
+                else {
+                    //res.json(result);
+                    res.send(res.json(result));
+                }
+            }
+        );
+    });
     
 });
 
