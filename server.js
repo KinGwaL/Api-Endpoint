60b6aa1d383ea28dff1bae3a14d59fa95f6cfc3c
King Wa Lai <king_lai@outlook.com>
2020-06-12 13:51:40 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,20 +7,30 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Error
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(400).send(err.message);
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 app.get('/api-endpoint', function(request, response) {
-    var name = request.query.name;
-    
-      var options = { 
-            title: 'Title',
-            imageUrl: 'https://image.flaticon.com/icons/png/512/61/61456.png',
-            Contact: [ {id: 12345, name: 'King Lai'},{id: 12346, name: 'King King Lai'}],
+    //let name = request.query.name;
+  
+      let options = { 
+            title: "Title",
+            imageUrl: "https://image.flaticon.com/icons/png/512/61/61456.png",
+            Contact: [ {id: 12345, name: "King Lai"},{id: 12346, name: "King King Lai"}],
             show: true 
         };
     
-    response.send(options.json());
+    response.send(JSON.stringify(options));
     
-});
-
-app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
 });
