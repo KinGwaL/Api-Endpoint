6677f50a62d841b00fb2fa4aef847a3c1e1d6aa8
King Wa Lai <king_lai@outlook.com>
2020-06-12 17:33:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,11 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 app.get('/api-endpoint', function(request, response) {
-    //var nameString = request.query.name;
-  
+    var nameString = request.query.name;
+    var historyString = request.query.history;
+    
       var jsonContent = { 
-            title: "Title",
+            title: nameString,
             imageUrl: "https://image.flaticon.com/icons/png/512/61/61456.png",
             contact: [ {id: 12345, name: "King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"}],
             show: true 
