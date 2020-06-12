84a0eb358f60a6911f62807a942be9339d59d20e
King Wa Lai <king_lai@outlook.com>
2020-06-12 13:56:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
-});
-
 app.get('/api-endpoint', function(request, response) {
     //let name = request.query.name;
   
@@ -23,4 +19,9 @@ app.get('/api-endpoint', function(request, response) {
     
     response.send(JSON.stringify(options));
     
+});
+
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
