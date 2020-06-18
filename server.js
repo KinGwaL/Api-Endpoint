10b29354066d880fda633c3bdb067aa832e23402
King Wa Lai <king_lai@outlook.com>
2020-06-18 14:28:12 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,21 +7,33 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
+// https://your-heroku-app-name.herokuapp.com/api-endpoint
 app.get('/api-endpoint', function(request, response) {
-    var nameString = request.query.name;
-    var historyString = request.query.history;
     
       var jsonContent = { 
-            title: nameString,
+            title: "title",
             imageUrl: "https://image.flaticon.com/icons/png/512/61/61456.png",
-            contact: [ {id: 12345, name: "King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"},{id: 12346, name: "King King Lai"}],
+            contact: [ {id: 12345, name: "King Lai"},{id: 12346, name: "King Lai"}],
+            show: true 
+        };
+    
+    response.send(JSON.parse(JSON.stringify(jsonContent)));
+});
+
+//https://your-heroku-app-name.herokuapp.com/api-endpoint-parameter?tite=ABCDE
+app.get('/api-endpoint-parameter', function(request, response) {
+    var titleRequest = request.query.name;
+    
+      var jsonContent = { 
+            title: titleRequest,
+            imageUrl: "https://image.flaticon.com/icons/png/512/61/61456.png",
+            contact: [ {id: 12345, name: "King Lai"},{id: 12346, name: "King King Lai"}],
             show: true 
         };
 
     response.send(JSON.parse(JSON.stringify(jsonContent)));
     
 });
-
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
