f6a511dfc8a101fa6beb50e6241f791129b68890
James <james.ellis@vonage.com>
2022-02-05 17:57:56 +0000

    added process order

--- server.js
+++ server.js
@@ -7,32 +7,17 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-// https://your-heroku-app-name.herokuapp.com/api-endpoint
-app.get('/api-endpoint', function(request, response) {
-    
-      var jsonContent = { 
-            title: "title",
-            imageUrl: "https://image.flaticon.com/icons/png/512/61/61456.png",
-            contact: [ {id: 12345, name: "King Lai"},{id: 12346, name: "King Lai"}],
-            show: true 
-        };
-    
-    response.send(JSON.parse(JSON.stringify(jsonContent)));
+
+//CPQ_PROCESSORDER
+app.post('/vorder/v1/orders', function(request, response) {
+    var jsonContent = {}; //returns empty body
+    response.send(JSON.parse(JSON.stringify(jsonContent))); 
 });
 
-//https://your-heroku-app-name.herokuapp.com/api-endpoint-parameter?tite=ABCDE
-app.get('/api-endpoint-parameter', function(request, response) {
-    var titleRequest = request.query.title; // Change .title / .name or any text based on your parameter name
-    
-      var jsonContent = { 
-            title: titleRequest,
-            imageUrl: "https://image.flaticon.com/icons/png/512/61/61456.png",
-            contact: [ {id: 12345, name: "King Lai"},{id: 12346, name: "King Lai"}],
-            show: true 
-        };
-
-    response.send(JSON.parse(JSON.stringify(jsonContent)));
-    
+//CPQ_PROCESSORDER_V2
+app.post('/vorder/v2/orders', function(request, response) {
+    var jsonContent = {}; //returns empty body
+    response.send(JSON.parse(JSON.stringify(jsonContent))); 
 });
 
 app.listen(app.get('port'), function () {
