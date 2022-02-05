929e2ce8e7493b850398938f281c189d160e22c0
James <james.ellis@vonage.com>
2022-02-05 20:46:12 +0000

    processorder request handler

--- server.js
+++ server.js
@@ -1,25 +1,76 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var pg = require('pg');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const got = require('got'); //use got for callouts 
+
 
 app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
+const ENDPOINT_BASE_URL = 'https://vonage--turkey.my.salesforce.com/services/apexrest';
+
 
 //CPQ_PROCESSORDER
-app.post('/vorder/v1/orders', function(request, response) {
-    var jsonContent = {}; //returns empty body
-    response.send(JSON.parse(JSON.stringify(jsonContent))); 
+app.post('/vorder/v1/orders', function (request, response) {
+
+    /***
+     * example request
+     * 
+     * [{"vOrderId":"a127b0df-4a83-4efc-aeae-3d540998be4e","vonageMRR":"57.97","salesContact":{"emailAddress":"q2cprodmail@gmail.com"},"paymentTerms":"Due on receipt","paymentMethodType":"CreditCard","paymentMethodId":"8ad098317e33d259017e44f019270255","orderType":"add-location","orderCorrelationId":"aDY02000000LP7EGAW","configuration":null,"callbackUrl":"Vbc_Process_Order_Complete__e","billingLocation":{"contact":{"phoneNumber":"7324444728","lastName":"Testposlt1","firstName":"Testposlt1","emailAddress":"sfdcvon@gmail.com"},"address":{"province":"CA","postalCode":"95695","jurisdictionCode":"424100","countryCode":"US","city":"WOODLAND","addressLine2":null,"addressLine1":"23 W MAIN ST"}},"apiSource":"SFDC","account":{"vParentAccountId":null,"vAccountId":null,"users":[{"thirdPartyIdentity":{"username":null,"providerIdentityId":null,"provider":null},"role":"ACCOUNT_SUPER_USER","receiveAlerts":null,"phoneNumber":null,"mobileNumber":null,"loginName":"Testposlt1","lastName":"Testposlt1","homeNumber":null,"firstName":"Testposlt1","faxNumber":null,"emailAddress":"sfdcvon@gmail.com","allowSetCallerId":null,"allowLogin":null}],"trial":null,"timezone":null,"status":"Pending","primaryLocation":false,"poNumber":"poslt1","parentAccountType":"Direct","parentAccountName":"VBS","leadId":null,"isTrial":null,"demoAccount":false,"crmAccountId":"0010200000EroCKAAZ","createDate":"2022-01-10","contract":{"startDate":"2022-01-24","endDate":"2023-01-23","contractTerm":12},"activationDate":"2022-01-24","accountType":"End-Customer","accountName":"testposlt1:Woodland"}}]
+     */
+
+    if(request.body){
+        var orderData = JSON.parse(request.body);
+ 
+        var vOrderIds = [];
+        for( key in orderData){
+            vOrderIds.push(orderData[key].vOrderId);
+        }
+        var jsonContent = {
+            message: 'mock process order response, for orderCorrelationId: '+orderCorrelationId+' extracted these vOrderIds : '+vOrderIds
+        };
+        response.send(JSON.parse(JSON.stringify(jsonContent)));
+    }else{
+        response.send({});
+    }
+
+    //executeCompletionCallouts( vOrderIds );
 });
 
 //CPQ_PROCESSORDER_V2
-app.post('/vorder/v2/orders', function(request, response) {
+app.post('/vorder/v2/orders', function (request, response) {
     var jsonContent = {}; //returns empty body
-    response.send(JSON.parse(JSON.stringify(jsonContent))); 
+    response.send(JSON.parse(JSON.stringify(jsonContent)));
 });
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
+
+function executeCompletionCallouts(  vOrderIds ) {
+
+    var headers = {
+        "Content-Type": "application/json; charset=utf-8",
+        "Authorization": "Basic NGEwMGZmMjItY2NkNy0xMWUzLTk5ZDUtMDAwYzI5NDBlNjJj"
+      };
+    var body = {"ShadowQuoteId":vOrderIds[0],"HDAPAccountId":"900001"};
+    var calloutOptions = {
+        url: ENDPOINT_BASE_URL + '/VonShadowQuoteServices',
+        headers: headers
+    }
+    //make the new HDAP Account Id call to sfdc
+    const {
+        body,
+        statusCode
+    } = await got( calloutOptions );
+    if (statusCode !== 200 || body.error) {
+        throw new Error(body.error || 'error calling sfdc with new hdap account id');
+    }
+
+    //pause, then
+    //make the new Zuora Account Id call to sfdc
+
+    //pause, then
+    //make the updateSQStatus call to sfdc
+}
