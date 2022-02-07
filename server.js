09ee48e43f535a43c688b5c5b7cf3657772e1c0e
James <james.ellis@vonage.com>
2022-02-07 08:30:08 +0000

    add aat & int

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 const axios = require('axios').default;
-const logger = require('pino')();
 const fs = require('fs');
 const querystring = require('querystring');
 const moment = require('moment');
 const jwt = require('jsonwebtoken');
 var path = require("path");
 var jsforce = require("jsforce");
-const { 
-    v1: uuidv1,
-    v4: uuidv4,
-  } = require('uuid');
+const uuidv4 = require('uuid');
 
 
 //setup the server
@@ -54,50 +50,33 @@ var token_url = new URL('/services/oauth2/token', ORG_CONFIG.basePath).toString(
 
 
 
-//CONFIGURE ROUTES
+//INTERFACE
 
 
 //CPQ_PROCESSORDER
 app.post('/vorder/v1/orders', function (request, response) {
-    logger.info('/vorder/v1/orders called');
-    logger.info(request.body);
-    /***
-     * example request
-     * 
-     * [{"vOrderId":"a127b0df-4a83-4efc-aeae-3d540998be4e","vonageMRR":"57.97","salesContact":{"emailAddress":"q2cprodmail@gmail.com"},"paymentTerms":"Due on receipt","paymentMethodType":"CreditCard","paymentMethodId":"8ad098317e33d259017e44f019270255","orderType":"add-location","orderCorrelationId":"aDY02000000LP7EGAW","configuration":null,"callbackUrl":"Vbc_Process_Order_Complete__e","billingLocation":{"contact":{"phoneNumber":"7324444728","lastName":"Testposlt1","firstName":"Testposlt1","emailAddress":"sfdcvon@gmail.com"},"address":{"province":"CA","postalCode":"95695","jurisdictionCode":"424100","countryCode":"US","city":"WOODLAND","addressLine2":null,"addressLine1":"23 W MAIN ST"}},"apiSource":"SFDC","account":{"vParentAccountId":null,"vAccountId":null,"users":[{"thirdPartyIdentity":{"username":null,"providerIdentityId":null,"provider":null},"role":"ACCOUNT_SUPER_USER","receiveAlerts":null,"phoneNumber":null,"mobileNumber":null,"loginName":"Testposlt1","lastName":"Testposlt1","homeNumber":null,"firstName":"Testposlt1","faxNumber":null,"emailAddress":"sfdcvon@gmail.com","allowSetCallerId":null,"allowLogin":null}],"trial":null,"timezone":null,"status":"Pending","primaryLocation":false,"poNumber":"poslt1","parentAccountType":"Direct","parentAccountName":"VBS","leadId":null,"isTrial":null,"demoAccount":false,"crmAccountId":"0010200000EroCKAAZ","createDate":"2022-01-10","contract":{"startDate":"2022-01-24","endDate":"2023-01-23","contractTerm":12},"activationDate":"2022-01-24","accountType":"End-Customer","accountName":"testposlt1:Woodland"}}]
-     */
-    var resBody = {};
-    if (request.body) {
-        var orderData = request.body;
-
-        var vOrderIds = [];
-        for (key in orderData) {
-            vOrderIds.push(orderData[key].vOrderId);
-        }
-        resBody = {
-            message: 'mock process order response, extracted these vOrderIds : ' + vOrderIds
-        };
-        response.send(JSON.parse(JSON.stringify(resBody)));
-    } else {
-        resBody = {
-            message: 'mock process order response'
-        };
-        response.send(JSON.parse(JSON.stringify(resBody)));
-    }
-
-    executeQuoteCompletionCallouts(vOrderIds);
+    console.log('/vorder/v1/orders called');
+    console.log(request.body);
+    doCPQProcessOrder(true, request, response);
 });
 
 //CPQ_PROCESSORDER_V2
 app.post('/vorder/v2/orders', function (request, response) {
-    var jsonContent = {}; //returns empty body
-    response.send(JSON.parse(JSON.stringify(jsonContent)));
+    console.log('/vorder/v2/orders called');
+    console.log(request.body);
+    doCPQProcessOrder(true, request, response);
+});
+
+//CPQ_AAT_PROCESSORDER
+app.post('/vorder/{ACCOUNT_NUMBER}/v1/orders', function (request, response) {
+    console.log('CPQ_AAT_PROCESSORDER - /vorder/{ACCOUNT_NUMBER}/v1/orders called');
+    console.log(request.body);
+    doCPQProcessOrder(false, request, response);
 });
 
 
 
-
-//SERVER LISTENER
+//LISTENER
 
 
 app.listen(app.get('port'), function () {
@@ -119,53 +98,79 @@ app.listen(app.get('port'), function () {
 });
 
 
-//API IMPLEMENTATIONS
+//IMPLEMENTATIONS
 
+function doCPQProcessOrder(isNewLogo, request, response) {
+    var resBody = {};
+    if (request.body) {
+        var orderData = request.body;
 
-async function executeQuoteCompletionCallouts(vOrderIds) {
+        var vOrderIds = [];
+        for (key in orderData) {
+            vOrderIds.push(orderData[key].vOrderId);
+        }
+        resBody = {
+            message: 'mock process order response, extracted these vOrderIds : ' + vOrderIds
+        };
+        response.send(JSON.parse(JSON.stringify(resBody)));
+    } else {
+        resBody = {
+            message: 'mock process order response'
+        };
+        response.send(JSON.parse(JSON.stringify(resBody)));
+    }
+
+    executeQuoteCompletionCallouts(isNewLogo, vOrderIds);
+}
+
+async function executeQuoteCompletionCallouts(isNewLogo, vOrderIds) {
 
     var accountNumber = 900001;
 
     if (sfdc_access_token) {
 
         for (key in vOrderIds) {
-            //TODO, incrementing accountnumbers... use simple mongodb table or query the org..
-            var requestBody = {
-                "ShadowQuoteId": vOrderIds[key],
-                "HDAPAccountId": accountNumber
-            };
-            var uri = '/VonShadowQuoteServices/';
-            var conn = new jsforce.Connection({
-                instanceUrl: sfdc_instance_url,
-                accessToken: sfdc_access_token
-            });
 
-            conn.apex.post(uri, requestBody, function (err, res) {
-                if (err) {
-                    return console.error(err);
-                }
-                console.log('VonShadowQuoteServices, ' + vOrderIds[key] + ' : set hdapid response: ', res);
-            });
+            if (isNewLogo) {
+                //TODO, incrementing accountnumbers... use simple mongodb table or query the org..
+                var requestBody = {
+                    "ShadowQuoteId": vOrderIds[key],
+                    "HDAPAccountId": accountNumber
+                };
+                var uri = '/VonShadowQuoteServices/';
+                var conn = new jsforce.Connection({
+                    instanceUrl: sfdc_instance_url,
+                    accessToken: sfdc_access_token
+                });
 
-            //pause
-            await sleep(1000);
-            //then
-            //make the new Zuora Account Id call to sfdc
-            var zuoraId = uuidv4();
-            requestBody = {
-                "ShadowQuoteId": vOrderIds[key],
-                "ZuoraAccountId": zuoraId
-            };
-            conn.apex.post(uri, requestBody, function (err, res) {
-                if (err) {
-                    return console.error(err);
-                }
-                console.log('VonShadowQuoteServices, ' + vOrderIds[key] + ' : set zAccountId response: ', res);;
-            });
+                conn.apex.post(uri, requestBody, function (err, res) {
+                    if (err) {
+                        return console.error(err);
+                    }
+                    console.log('VonShadowQuoteServices, ' + vOrderIds[key] + ' : set hdapid response: ', res);
+                });
 
-            //pause,
-            await sleep(1000);
-            //then
+                //pause
+                await sleep(1000);
+                //then
+                //make the new Zuora Account Id call to sfdc
+                var zuoraId = uuidv4();
+                requestBody = {
+                    "ShadowQuoteId": vOrderIds[key],
+                    "ZuoraAccountId": zuoraId
+                };
+                conn.apex.post(uri, requestBody, function (err, res) {
+                    if (err) {
+                        return console.error(err);
+                    }
+                    console.log('VonShadowQuoteServices, ' + vOrderIds[key] + ' : set zAccountId response: ', res);;
+                });
+
+                //pause,
+                await sleep(1000);
+                //then
+            }
+
             //make the updateSQStatus call to sfdc
             requestBody = {
                 "ShadowQuoteId": vOrderIds[key],
@@ -195,6 +200,6 @@ async function executeQuoteCompletionCallouts(vOrderIds) {
 
 function sleep(ms) {
     return new Promise((resolve) => {
-      setTimeout(resolve, ms);
+        setTimeout(resolve, ms);
     });
-  }
+}
